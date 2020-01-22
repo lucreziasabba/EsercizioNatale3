eec93a53bb376d60503b4e4bf080958770112bff
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-01-22 13:21:50 +0100

    fine

--- WpfEsercizioNatale3/MainWindow.xaml.cs
+++ WpfEsercizioNatale3/MainWindow.xaml.cs
@@ -20,35 +20,56 @@ namespace WpfEsercizioNatale3
     /// </summary>
     public partial class MainWindow : Window
     {
+        private TextBox[] x;
         public MainWindow()
         {
             InitializeComponent();
+            x = new TextBox[] { txtNum1, txtNum2, txtNum3, txtNum4, txtNum5, txtNum6 };
         }
 
         private void ON_Click(object sender, RoutedEventArgs e)
         {
-            txtNum1.IsEnabled = true;
-            txtNum2.IsEnabled = true;
-            txtNum3.IsEnabled = true;
-            txtNum4.IsEnabled = true;
-            txtNum5.IsEnabled = true;
-            txtNum6.IsEnabled = true;
-            btnOn.Content = "OFF";
-        }
-        private void OFF_Click(object sender, RoutedEventArgs e)
-        {
-            txtNum1.IsEnabled = false;
-            txtNum2.IsEnabled = false;
-            txtNum3.IsEnabled = false;
-            txtNum4.IsEnabled = false;
-            txtNum5.IsEnabled = false;
-            txtNum6.IsEnabled = false;
-            btnOn.Content = "ON";
+            if (btnOn.Content.ToString() == "ON")
+            {
+                txtNum1.IsEnabled = true;
+                txtNum2.IsEnabled = true;
+                txtNum3.IsEnabled = true;
+                txtNum4.IsEnabled = true;
+                txtNum5.IsEnabled = true;
+                txtNum6.IsEnabled = true;
+                btnCalcola.IsEnabled = true;
+                btnOn.Content = "OFF";
+            }
+            else
+            {
+                txtNum1.IsEnabled = false;
+                txtNum2.IsEnabled = false;
+                txtNum3.IsEnabled = false;
+                txtNum4.IsEnabled = false;
+                txtNum5.IsEnabled = false;
+                txtNum6.IsEnabled = false;
+                btnCalcola.IsEnabled = false;
+                btnOn.Content = "ON";
+            }
         }
 
         private void Calcola_Click(object sender, RoutedEventArgs e)
         {
-
+            int pari = 0;
+            int dispari = 0;
+            for (int i = 0; i < x.Length; i++)
+            {
+                int num = int.Parse(x[i].Text);
+                if (num % 2 == 0)
+                {
+                    pari++;
+                }
+                else
+                {
+                    dispari++;
+                }
+            }
+            lblRisultato.Content = $"I numeri pari sono {pari}, i numeri dispari sono {dispari}";
         }
     }
 }
